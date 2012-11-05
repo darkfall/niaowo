98616e8a42c18066f657634fb2b013bc481b2dae
Robert Bu <darkfall3@gmail.com>
2012-11-04 19:23:00 -0600

    minor adjustments

--- niaowo/Settings.h
+++ niaowo/Settings.h
@@ -2,11 +2,12 @@
 //  Settings.h
 //  niaowo
 //
-//  Created by Robert Bu on 10/28/12.
+//  Created by Robert Bu on 11/4/12.
 //  Copyright (c) 2012 Robert Bu. All rights reserved.
 //
 
-#include <Cocoa/Cocoa.h>
+#import <Foundation/Foundation.h>
 
-static NSString* API_KEY = @"dda41387da09431d87e3d383108150b1";
-static NSString* API_SECRET = @"d08b997fe9b94f89b23e91d5552dbcb2";
+@protocol Settings <NSObject>
+
+@end
